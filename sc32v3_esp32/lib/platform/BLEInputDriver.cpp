756ea9a72efd9d74d8ea1d86a00276344b589e17
Oleg Lytvynenko (Personal) <oleg.v.lytvynenko@gmail.com>
2021-06-21 13:08:10 +0300

    invert x axis

--- sc32v3_esp32/lib/platform/BLEInputDriver.cpp
+++ sc32v3_esp32/lib/platform/BLEInputDriver.cpp
@@ -86,7 +86,7 @@ bool BLEInputDriver::ProcessInput(HexModel* model)
 	else
 	{
 		state.buttons = incomingData.buttons;
-		state.axis_data[0] = (int16_t)((incomingData.axis_data[0]-2048) / 12);
+		state.axis_data[0] = -(int16_t)((incomingData.axis_data[0]-2048) / 12);
 		state.axis_data[1] = -(int16_t)((incomingData.axis_data[1]-2048) / 12);
 		state.axis_data[2] = (int16_t)((incomingData.axis_data[2]-2048) / 12);
 		state.axis_data[3] = -(int16_t)((incomingData.axis_data[3]-2048) / 12);
